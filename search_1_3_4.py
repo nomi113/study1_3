54522cd519da2326625b779d5a45079f256be1f7
nomi113 <80982518+nomi113@users.noreply.github.com>
2021-04-11 19:52:16 +0900

    Add files via upload

---
+++ search_1_3_4.py
@@ -0,0 +1,34 @@
+
+### 検索ツールサンプル
+### これをベースに課題の内容を追記してください
+
+# 検索ソース
+# source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+# csv読み込み
+
+
+
+### 検索ツール
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+
+    ### ここに検索ロジックを書く
+    import csv
+    with open('kimetsu.csv','r')as csv_file:
+        reader=csv.reader(csv_file)
+        for source in reader:
+            print(source)
+
+        
+    if word in source:
+        print("{}が見つかりした".format(word))
+        
+    else:
+        print('存在しません')
+        with open('kimetsu.csv','a')as csv_file:
+            print(word,file=csv_file)
+        
+        
+
+if __name__ == "__main__":
+    search()
